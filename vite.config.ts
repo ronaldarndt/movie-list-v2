d1323b5f71089b1c439350b981d0ad26e30a7e6e
ronaldarndt <ronaldarndtf@gmail.com>
2024-01-17 23:56:17 -0300

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import preact from "@preact/preset-vite";
 import UnoCSS from "unocss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import EnvironmentPlugin from "vite-plugin-environment";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [UnoCSS(), preact(), EnvironmentPlugin("all", { prefix: "" })],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [UnoCSS(), preact(), EnvironmentPlugin("all", { prefix: "" })],
+    define: {
+      "process.env": { ...env, ...process.env },
+    },
+  };
 });
