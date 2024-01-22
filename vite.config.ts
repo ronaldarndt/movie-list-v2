fa1c42c7c2474959adb37198fd9e0c6d40a3910a
ronaldarndt <ronaldarndtf@gmail.com>
2024-01-21 21:53:09 -0300

    feat: add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,19 @@ import preact from "@preact/preset-vite";
 import UnoCSS from "unocss/vite";
 import { defineConfig, loadEnv } from "vite";
 import EnvironmentPlugin from "vite-plugin-environment";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    plugins: [UnoCSS(), preact(), EnvironmentPlugin("all", { prefix: "" })],
+    plugins: [
+      UnoCSS(),
+      preact(),
+      EnvironmentPlugin("all", { prefix: "" }),
+      VitePWA({ registerType: "autoUpdate" }),
+    ],
     define: {
       "process.env": { ...env, ...process.env },
     },
